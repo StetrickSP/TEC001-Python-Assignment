5bb301c8a9101084a43038ee5fed235bffdbceac
StetrickSP <141539538+StetrickSP@users.noreply.github.com>
2026-01-13 13:34:47 +0700

    Add files via upload

--- lock_code.py
+++ lock_code.py
@@ -1,7 +1,15 @@
 import random
 
-code_3 = [random.randint(0, 9) for _ in range(3)]
-code_4 = [random.randint(1, 6) for _ in range(4)]
+code_3 = [str(random.randint(0, 9)) for _ in range(3)]
+code_4 = [str(random.randint(1, 6)) for _ in range(4)]
+ans_3 = ''
+ans_4 = ''
 
-print("3-digit code:", code_3)
-print("4-digit code:", code_4)
+for i in code_3:
+    ans_3 += i
+
+for i in code_4:
+    ans_4 += i
+
+print("3-digit code:", ans_3)
+print("4-digit code:", ans_4)
